4e60a59b646e284b3b248de47832900900dcc61e
Nutdanai <earth.zaza2554@gmail.com>
2024-06-10 23:00:25 +0700

    create-docker

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.8
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
+EXPOSE 5000
+CMD ["python", "./app/main.py"]
--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from recomender import recommend_movies
 import pandas as pd
 import os
 
+
 app = Flask(__name__)
 
 # Configure logging Refference from this : https://docs.python.org/3/howto/logging-cookbook.html
@@ -75,4 +76,4 @@ def get_features():
     return jsonify({'features': [{'histories': user_history}]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=False)
